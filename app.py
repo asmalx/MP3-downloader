ac46e1f88ea70a9c9e1dcd417d7e877c373e54f4
asmalx <Aleksandr1871@gmai.com>
2021-08-06 13:59:06 +0300

    bug fized2

--- app.py
+++ app.py
@@ -6,28 +6,24 @@ app.config['SECRET_KEY'] = '2c7f07064e05560b9745cfef4fda2b7307aaf3f7'
 
 # videos vost for current client
 videos_list, urls = [], []
-ptr = 0
 # current video title to download
 current_title = ""
 
 @app.route('/') 
 @app.route('/index')
 def index():
-    global videos_list, urls, current_title, ptr
+    global videos_list, urls, current_title
     # if main page - clear data
     videos_list = urls = []
     current_title = ""
-    ptr = 0
     return render_template("index.html", tracks=False)
 
 
 @app.route('/submit', methods=["POST"])
 def submit():
-    global videos_list, urls, ptr, current_title
+    global videos_list, urls, current_title
     # reset all old videos 
-    videos_list = urls = []
-    # at the start, process first 5 videos
-    ptr = 0
+    videos_list, urls = [], []
     # no chosen video 
     current_title = ""
 
@@ -40,7 +36,7 @@ def submit():
     # entered text
     else:
         urls = functions.get_linklist_by_title(title)
-        for i in range(ptr, min(ptr+5, len(urls))):
+        for i in range(len(urls)):
             video = functions.search_video_data(urls[i])
             video['number'] = i+1
             videos_list.append(video)           
@@ -48,11 +44,12 @@ def submit():
 
 @app.route('/download', methods=["POST"])
 def download():
-    global current_title
+    global current_title, videos_list
+    
     action, video_number = request.form["button"], int(request.form["data"])
     #print("Entered action:", action, "entered video id", video_number)
 
-    # validate nubber
+    # validate number
     if video_number < 0 or video_number > len(videos_list): # do nothing, for safety
         return render_template("index.html", videos = videos_list)
 
